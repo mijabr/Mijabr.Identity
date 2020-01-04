e3677a60af198601a67b75502f0b4f50fa166ec4
Brydie <Michaelb@attachesoftware.com>
2020-01-04 17:36:10 +1100

    initial

---
+++ lib/Mijabr.Identity/Quickstart/Routes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mijabr.Identity.Quickstart
+{
+    public class Routes
+    {
+        public static string Default => "~/";
+        //public static string Default => "~/identity";
+    }
+}
